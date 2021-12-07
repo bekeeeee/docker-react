e2cafb3fb665a28de5414f30515d9a251ea855ec
bekeeeee <m.ragab9521@gmail.com>
2021-12-08 01:50:16 +0200

    Change travis file

--- test.js
+++ test.js
@@ -1,42 +1,48 @@
-var fullname = "Donald Duck";
-var obj = {
-  fullname: "Donald Trump",
-  prop: {
-    fullname: "Mickey Mouse",
-    getFullname: function () {
-    //   console.log(this);
+// var fullname = "Donald Duck";
+// var obj = {
+//   fullname: "Donald Trump",
+//   prop: {
+//     fullname: "Mickey Mouse",
+//     getFullname: function () {
+//     //   console.log(this);
 
-      return this.fullname;
-    },
-  },
-  getFirstName: function () {
-    // console.log(this);
+//       return this.fullname;
+//     },
+//   },
+//   getFirstName: function () {
+//     // console.log(this);
 
-    return this.fullname
-  },
-  getMyName: () => {
-    // console.log(this);
-    return this.fullname;
-  },
+//     return this.fullname
+//   },
+//   getMyName: () => {
+//     // console.log(this);
+//     return this.fullname;
+//   },
 
-  getLastName: (function () {
-    return this.fullname;
-  })(),
-};
+//   getLastName: (function () {
+//     return this.fullname;
+//   })(),
+// };
 
-console.log(obj.prop.getFullname());
-console.log(obj.getFirstName());
-console.log(obj.getMyName());
-console.log(obj.getLastName);
+// console.log(obj.prop.getFullname());
+// console.log(obj.getFirstName());
+// console.log(obj.getMyName());
+// console.log(obj.getLastName);
 
-// for(var i=0; i<3;i++){
-//     setTimeout(function (){
-//         console.log(i)
-//     },100+i)
-// }
+// // for(var i=0; i<3;i++){
+// //     setTimeout(function (){
+// //         console.log(i)
+// //     },100+i)
+// // }
 
-// (function(){
-//     var a= b=5
-// })
-//         // console.log(b)
-//         console.log(a)
+// // (function(){
+// //     var a= b=5
+// // })
+// //         // console.log(b)
+// //         console.log(a)
+
+function newUser(name, isDeveoloper) {
+  return { name, isDeveoloper };
+}
+
+console.log(newUser("beke","true"))
